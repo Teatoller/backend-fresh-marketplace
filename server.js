673170a8cf97790824e3ddc40ff1173e10d3066e
stevenennis <seotieno@safaricom.co.ke>
2024-07-11 12:46:26 +0300

    add server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/fresh-marketplace', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
